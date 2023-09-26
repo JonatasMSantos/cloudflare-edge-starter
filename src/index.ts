fffefa237a5714a81099bfaedd2569182a0f76d9
Jônatas Santos <jts.msantos@gmail.com>
2023-09-26 14:33:58 -0300

    🚀 first commit

---
+++ src/index.ts
@@ -0,0 +1,65 @@
+import { Redis } from '@upstash/redis/cloudflare'
+
+/**
+ * Welcome to Cloudflare Workers! This is your first worker.
+ *
+ * - Run `npm run dev` in your terminal to start a development server
+ * - Open a browser tab at http://localhost:8787/ to see your worker in action
+ * - Run `npm run deploy` to publish your worker
+ *
+ * Learn more at https://developers.cloudflare.com/workers/
+ */
+
+export interface Env {
+	UPSTASH_REDIS_REST_URL: string;
+	UPSTASH_REDIS_REST_TOKEN: string;
+	UPSTASH_DISABLE_TELEMETRY?: string | undefined;
+
+	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
+	// MY_KV_NAMESPACE: KVNamespace;
+	//
+	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
+	// MY_DURABLE_OBJECT: DurableObjectNamespace;
+	//
+	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
+	// MY_BUCKET: R2Bucket;
+	//
+	// Example binding to a Service. Learn more at https://developers.cloudflare.com/workers/runtime-apis/service-bindings/
+	// MY_SERVICE: Fetcher;
+	//
+	// Example binding to a Queue. Learn more at https://developers.cloudflare.com/queues/javascript-apis/
+	// MY_QUEUE: Queue;
+}
+
+export default {
+	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+		const { pathname } = new URL(request.url)
+		
+		console.log(env)
+		const redis = Redis.fromEnv(env);
+
+		switch (pathname) {
+			case '/write': {
+				const count = await redis.incr('count')
+				return new Response(JSON.stringify({ count }), {
+					headers: {
+						'Content-type': 'application/json'
+					}
+				})
+			}
+
+			case '/read': {
+				const count = await redis.get('count')
+				return new Response(JSON.stringify({ count }), {
+					headers: {
+						'Content-type': 'application/json'
+					}
+				})
+			}
+
+			default: {
+				return new Response(null, { status: 404 })
+			}
+		}
+	},
+};
